1b4407e38ad41b5f85d60611fbb65159149dfa01
mikeaig4real <mikeaig4realonreal@gmail.com>
2023-08-17 11:33:05 +0100

    more on db schemas

--- models.py
+++ models.py
@@ -184,3 +184,8 @@ class Project(db.Model):
             "description": project.description,
             "user_id": project.user_id,
         }
+        
+        # Generally is more expensive to have to cumulate counts in a db query e.g 
+        # say a table user exists and a user would have followers, design wise its better to have a column called followers_count
+        # that increments or decrements so it is not expensive to query the db for the number of followers a user has
+        # to do that you would need triggers, doc on triggers at [https://www.postgresql.org/docs/9.1/sql-createtrigger.html]
